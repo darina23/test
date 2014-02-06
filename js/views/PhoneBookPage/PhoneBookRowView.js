25378a499e2be913ca8de36c55188a2cd25783f6
darina <daria.limanskaya@mrcplast.com>
2014-02-06 15:56:58 +0200

    remove

--- js/views/PhoneBookPage/PhoneBookRowView.js
+++
@@ -1,9 +0,0 @@
-var PhoneBookRowView = Backbone.View.extend({
-    tagName: "li",
-    className: "contact-row row",
-    events: {
-    },
-    render: function () {
-        return $(this.el).html(new EJS({url: '/js/templates/bookRow.ejs'}).render());
-    }
-})
